882355f8a31a03e619049f97059c40b36f6ece38
Sherlock0122 <sherlockcth@gmail.com>
2021-09-14 16:34:02 +0800

    update naming

---
+++ backend/pandanews/src/test/java/sg/smu/edu/cs203/pandanews/PandaNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.smu.edu.cs203.pandanews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
