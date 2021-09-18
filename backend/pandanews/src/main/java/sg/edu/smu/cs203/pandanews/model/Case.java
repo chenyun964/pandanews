30075ebb38618cde8aca8baa441b5c1f529dcbfe
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-09-18 19:55:04 +0800

    Create Case.java

---
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/model/Case.java
@@ -0,0 +1,13 @@
+package sg.edu.smu.cs203.pandanews.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Case {
+    private int id;
+
+    private String latitude;
+
+    private String longitude;
+
+}
