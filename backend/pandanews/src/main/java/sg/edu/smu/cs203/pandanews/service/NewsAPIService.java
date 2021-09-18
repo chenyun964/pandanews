8ace4a5b0cc8e8d791a24a818018bdf833e287aa
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-09-18 14:44:13 +0800

    Implementation of newsAPIService

--- backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/service/NewsAPIService.java
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/service/NewsAPIService.java
@@ -1,10 +1,14 @@
 package sg.edu.smu.cs203.pandanews.service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
+import sg.edu.smu.cs203.pandanews.entities.*;
 
 import java.util.Arrays;
 
@@ -23,20 +27,22 @@ public class NewsAPIService implements APICallingService {
 
     @Override
 //    public ResponseEntity<?> apiCall() {
-    public ResponseEntity<String> apiCall() {
+    public ResponseEntity<?> apiCall() {
         System.out.println(String.format(endpoint + "?cc=%s&q=%s", countryCode, phase));
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.set("Ocp-Apim-Subscription-Key", ApiKey);
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<Object> entity = new HttpEntity<Object>(new Object(),
-                headers);
+        HttpEntity entity = new HttpEntity(headers);
         try {
-            ResponseEntity r = restTemplate.exchange(
-                    String.format(endpoint + "?cc=%s&q=%s", countryCode, phase), HttpMethod.GET, entity,
-                    String.class);
-            System.out.println(r.getBody());
-            return r;
+            ObjectMapper mapper = new ObjectMapper()
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, false);
+            newsListDAO deserializedObj = mapper.
+                    readValue(restTemplate.exchange(String.format(endpoint + "?cc=%s&q=%s", countryCode, phase), HttpMethod.GET,
+                            entity, String.class, 1).getBody(), newsListDAO.class);
+            System.out.println(deserializedObj.get_type());
+            return ResponseEntity.ok(deserializedObj);
         } catch (Exception exception) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
         }
