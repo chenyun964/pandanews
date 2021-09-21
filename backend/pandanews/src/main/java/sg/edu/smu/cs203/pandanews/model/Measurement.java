4de3acfa17b6f579ac6109528334d119a372fec7
yi.chen <yi.chen.2020@sis.smu.edu.sg>
2021-09-21 11:02:47 +0800

    creation of db + DAO

---
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/model/Measurement.java
@@ -0,0 +1,61 @@
+package sg.edu.smu.cs203.pandanews.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Column;
+
+import lombok.*;
+
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Measurement {
+    @Id 
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    private String image_url;
+
+    @Setter
+    private String title;
+
+    @Setter
+    private String content;
+
+    // did not manage to find the admin table
+    @OneToMany(mappedBy = "xxx", cascade = CascadeType.All, orphanRemoval = true)
+    private List<Admin> xxx ;
+
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @PrePersist
+    public void logCreation() {
+        Date temp = new Date();
+        Object param = new java.sql.Timestamp(temp.getTime());
+        createdAt = (Date) param;
+        updatedAt = (Date) param;
+    }
+
+    @PreUpdate
+    public void logUpdate() {
+        Date temp = new Date();
+        Object param = new java.sql.Timestamp(temp.getTime());
+        updatedAt = (Date) param;
+    }
+    
+}
---
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/model/MeasurementDAO.java
@@ -0,0 +1,40 @@
+package sg.edu.smu.cs203.pandanews.model.news;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class MeasurementsDAO{
+    private String title;
+    private Image image;
+    private String description;
+    private DateTime date;
+
+    public MeasurementDAO(String title, Image image, String description, DateTime date){
+        this.title = title;
+        this.image = image;
+        this.description = description;
+        this.date = date;
+    }
+
+    public MeasurementDAO(){
+
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public Image getImage(){
+        return image;
+    }
+
+    public String getDescr(){
+        return description;
+    }
+
+    public DateTime getDate(){
+        return date;
+    }
+}
