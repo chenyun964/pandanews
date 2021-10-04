3349f1847ebb51392fbaa34f7ac2835943d9254b
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-10-04 14:28:00 +0800

    Update Config.js

--- frontend/src/config/Config.js
+++ frontend/src/config/Config.js
@@ -3,11 +3,11 @@ const API_HOSTS = process.env.REACT_APP_API_URL;
 //Authentication 
 const authenticate_api = API_HOSTS + '/authenticate';
 const register_api = API_HOSTS + '/register';
-const news_api = API_HOSTS + '/news'
+const news_list_api = API_HOSTS + '/news/list'
 
 export default{
     API_HOSTS,
     authenticate_api,
     register_api,
-    news_api
+    news_list_api
 }
