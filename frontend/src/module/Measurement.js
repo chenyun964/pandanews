204795d5f2e8ac5d7babc09796755fc87b48d8da
yi.chen <yi.chen.2020@sis.smu.edu.sg>
2021-10-05 12:00:51 +0800

    Completing of frontend without styling

--- frontend/src/module/Measurement.js
+++ frontend/src/module/Measurement.js
@@ -1,5 +1,19 @@
 import { Component } from 'react';
 import MeasurementModel from "../model/MeasurementModel";
+
+import React from 'react';
+import {
+    Header,
+    Item
+} from 'semantic-ui-react';
+import "../App.css";
+
+const style = {
+    h3: {
+      marginTop: '2em',
+      padding: '2em 0em',
+    }
+  }
 // import ReactDOM from "react-dom";
 // import { Col, Row, Container } from "@kunukn/react-bootstrap-grid";
 
@@ -27,15 +41,25 @@ class Measurement extends Component {
             <div className="meaInfographic">
                 <header className="App-header">
                     <div className="App-intro">
-                        <h2>Latest Measurements</h2>
-                        {this.state.measurements.map((m, i) =>{
+                        <Header as='h3' content='Latest Measurements' style={style.h3} textAlign='center' />
+                        {this.state.measurements.map((m, i) => {
                             return (<div key={m.id}>
-                                {m.title}
-                                {m.imageUrl}
-                                ({m.content})
-                                </div>
-                            )}
-                  )}
+                                <Item.Group>
+                                    <Item>
+                                    <Item.Image size='small' height="150px" src='https://react.semantic-ui.com/images/wireframe/image.png' />
+
+                                    <Item.Content>
+                                        <Item.Header as='a' >{m.title}</Item.Header>
+                                        <Item.Description >
+                                        <p>{m.content}</p>
+                                        </Item.Description>
+                                    </Item.Content>
+                                    </Item>
+                                </Item.Group>
+                            </div>
+                            )
+                        }
+                        )}
                     </div>
                 </header>
             </div>
