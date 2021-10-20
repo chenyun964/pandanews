2f52aaf3a594cf3e13729f417df590b4f8366bfb
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-10-21 00:40:33 +0800

    Update NewsDuplicationException.java

--- backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/exception/news/NewsDuplicationException.java
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/exception/news/NewsDuplicationException.java
@@ -3,6 +3,9 @@ package sg.edu.smu.cs203.pandanews.exception.news;
 public class NewsDuplicationException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
+    public NewsDuplicationException() {
+    }
+
     public NewsDuplicationException(String title) {
         super("News exists: " + title);
     }
