bb18ac9ee0718f2a57bc72bb0fa75a47e9d6da92
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-11-08 01:42:46 +0800

    Update with file uploading

--- backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/PandaNewsApplication.java
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/PandaNewsApplication.java
@@ -2,8 +2,13 @@ package sg.edu.smu.cs203.pandanews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import sg.edu.smu.cs203.pandanews.config.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class PandaNewsApplication {
 
 	public static void main(String[] args) {
