310ef6257dc5ec6257c28be1ecbc3fe7db621151
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-11-11 00:03:28 +0800

    update measurement and stats exception

--- backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/controller/MeasurementController.java
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/controller/MeasurementController.java
@@ -1,62 +1,53 @@
 package sg.edu.smu.cs203.pandanews.controller;
 
 
-import java.util.List;
-import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
+import org.springframework.web.bind.annotation.*;
+import sg.edu.smu.cs203.pandanews.exception.MeasurementNotFoundException;
 import sg.edu.smu.cs203.pandanews.model.Measurement;
 import sg.edu.smu.cs203.pandanews.service.measurement.MeasurementService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class MeasurementController {
     private MeasurementService measurementService;
 
-    public MeasurementController(MeasurementService ms){
+    public MeasurementController(MeasurementService ms) {
         this.measurementService = ms;
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/measurements")
-    public Measurement addMeasurement(@Valid @RequestBody Measurement measurement){
-        Measurement savedMeasurement = measurementService.addMeasurement(measurement);
-        // if (savedMeasurement ==  null) throw new BookExistsException(measurement.getTitle());
-        return savedMeasurement;
+    public Measurement addMeasurement(@Valid @RequestBody Measurement measurement) {
+        return measurementService.addMeasurement(measurement);
     }
 
     @GetMapping("/measurements")
-    public List<Measurement> getMeasurements(){
+    public List<Measurement> getMeasurements() {
         return measurementService.displayMeasurements();
     }
 
     @GetMapping("/measurements/{id}")
-    public Measurement getMeasurement(@PathVariable Long id){
+    public Measurement getMeasurement(@PathVariable Long id) {
         Measurement measurement = measurementService.getMeasurement(id);
 
-        if(measurement == null) 
-            return null;
+        if (measurement == null)
+            throw new MeasurementNotFoundException("Measurement not found");
         return measurementService.getMeasurement(id);
     }
 
     @PutMapping("/measurements/{id}")
-    public Measurement updateMeasurement(@PathVariable Long id, @RequestBody Measurement newMeasurement){
+    public Measurement updateMeasurement(@PathVariable Long id, @RequestBody Measurement newMeasurement) {
         Measurement measurement = measurementService.updateMeasurement(id, newMeasurement);
-        if(measurement == null) return null;
+        if (measurement == null) throw new MeasurementNotFoundException("Measurement not found");
         return measurement;
     }
 
     @DeleteMapping("/measurements/{id}")
-    public void deleteMeasurement(@PathVariable Long id){
+    public void deleteMeasurement(@PathVariable Long id) {
         // try{
         //     measurementService.deleteMeasurement(id);
         //  }catch(EmptyResultDataAccessException e) {
---
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/exception/statistics/StatisticNotFoundException.java
@@ -0,0 +1,17 @@
+package sg.edu.smu.cs203.pandanews.exception.statistics;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class StatisticNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public StatisticNotFoundException() {
+    }
+
+    public StatisticNotFoundException(String message) {
+        super("News Not Found");
+    }
+}
