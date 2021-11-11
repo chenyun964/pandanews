9742dfe4718a870322beb8efb5bce109d1c2c4f1
nineliejasongu1122 <nineliejasongu@gmail.com>
2021-11-11 19:28:21 +0800

    update slug and policy

--- backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/controller/PolicyController.java
+++ backend/pandanews/src/main/java/sg/edu/smu/cs203/pandanews/controller/PolicyController.java
@@ -1,106 +1,51 @@
 package sg.edu.smu.cs203.pandanews.controller;
 
-import java.util.List;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import sg.edu.smu.cs203.pandanews.exception.UnauthenticatedException;
-import sg.edu.smu.cs203.pandanews.exception.UnauthorizedUserException;
-import sg.edu.smu.cs203.pandanews.service.policy.PolicyService;
-import sg.edu.smu.cs203.pandanews.service.user.UserService;
+import org.springframework.web.bind.annotation.*;
+import sg.edu.smu.cs203.pandanews.exception.OrganisationNotFoundException;
 import sg.edu.smu.cs203.pandanews.model.Policy;
-import sg.edu.smu.cs203.pandanews.repository.UserRepository;
-import sg.edu.smu.cs203.pandanews.model.user.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.context.SecurityContextHolder;
+import sg.edu.smu.cs203.pandanews.service.policy.PolicyService;
+
+import java.util.List;
 
 @RestController
 public class PolicyController {
+    @Autowired
     private PolicyService policyService;
-    private UserService userService;
-    private UserRepository userRepo;
 
-    public PolicyController(PolicyService policyService){
-        this.policyService = policyService;
-    }
 
     @GetMapping("/organisation/{oid}/policies")
-    public List<Policy> getPolicies(@PathVariable Long oid) throws UnauthenticatedException, UnauthorizedUserException {
-        final UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        User user = userService.getUserByUsername(userDetails.getUsername());
-        if(user == null) throw new UnauthenticatedException();
-
-        if(!userRepo.findByOrganisationId(oid).contains(user)) throw new UnauthorizedUserException();
-
-        return policyService.listPolicies(oid);
+    public List<Policy> getPolicies(@PathVariable Long oid) {
+        List<Policy> result = policyService.listPolicies(oid);
+        if (result == null) throw new OrganisationNotFoundException();
+        return result;
     }
 
     @GetMapping("/organisation/{oid}/policies/{id}")
-    public Policy getPolicy(@PathVariable Long oid, @PathVariable Long id){
-        final UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        User user = userService.getUserByUsername(userDetails.getUsername());
-        if(user == null) throw new UnauthenticatedException();
-
-        if(!userRepo.findByOrganisationId(oid).contains(user)) throw new UnauthorizedUserException();
-
-        Policy policy = policyService.getPolicy(id);
-
-        if(policy == null) return null;
-        return policyService.getPolicy(id);
-
+    public Policy getPolicy(@PathVariable Long oid, @PathVariable Long id) {
+        Policy p = policyService.getPolicy(id, oid);
+        if (p == null) throw new OrganisationNotFoundException();
+        return p;
     }
-    
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/organisation/{oid}/policies")
-    public Policy addPolicy(@PathVariable Long oid, @RequestBody Policy policy){
-        final UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        User user = userService.getUserByUsername(userDetails.getUsername());
-        if(user == null) throw new UnauthenticatedException();
-
-        if(!userRepo.findByOrganisationId(oid).contains(user)) throw new UnauthorizedUserException();
-
-        return policyService.addPolicy(policy);
-    }
-
-    @PutMapping("/organisation/{oid}/policies/{id}")
-    public Policy updatePolicy(@PathVariable Long oid, @PathVariable Long id, @RequestBody Policy newPolicyInfo){
-        final UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        User user = userService.getUserByUsername(userDetails.getUsername());
-        if(user == null) throw new UnauthenticatedException();
-
-        if(!userRepo.findByOrganisationId(oid).contains(user)) throw new UnauthorizedUserException();
-
-        Policy policy = policyService.updatePolicy(id, newPolicyInfo);
-        if(policy == null) return null;
-        
-        return policy;
-    }
-
-    @DeleteMapping("/organisation/{oid}/policies/{id}")
-    public void deletePolicy(@PathVariable Long oid, @PathVariable Long id){
-        final UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        User user = userService.getUserByUsername(userDetails.getUsername());
-        if(user == null) throw new UnauthenticatedException();
-
-        if(!userRepo.findByOrganisationId(oid).contains(user)) throw new UnauthorizedUserException();
-
-        try {
-            policyService.deletePolicy(id);
-        } catch(EmptyResultDataAccessException e) {
-            // throw new PolicyNotFoundException(id);
+    public Policy addPolicy(@PathVariable Long oid, @RequestBody Policy policy) {
+        Policy result = policyService.addPolicy(policy, oid);
+        if (result == null) {
+            throw new OrganisationNotFoundException();
         }
+        return result;
+    }
+
+    @PutMapping("/organisation/policies/{id}")
+    public Policy updatePolicy(@PathVariable Long id, @RequestBody Policy newPolicyInfo) {
+        return policyService.updatePolicy(id, newPolicyInfo);
+    }
+
+    @DeleteMapping("/organisation/policies/{id}")
+    public void deletePolicy(@PathVariable Long id) {
+        policyService.deletePolicy(id);
     }
 }
